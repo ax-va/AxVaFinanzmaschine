271c3edc83831f0f042db623d11913c334f91912
ax-va <vasiliev.informatik@t-online.de>
2026-01-03 18:25:22 +0100

    update

--- core/quote.py
+++ core/quote.py
@@ -1,20 +1,15 @@
-import datetime
-
-
-class Quote:
-    def __init__(self) -> None:
-        self.data = {
-            "GB00BLD4ZM24": {
-                "2026-01-02 16:15": {
-                    "ETP": {
-                        "bid_eur": None,
-                        "ask_eur": None,
-                    },
-                    "ETH": {
-                        "bid_eur": 2610,
-                        "ask_eur": 2613,
-                        "local_high_eur": 4000,
-                    }
-                }
+quote = {
+    "GB00BLD4ZM24": {
+        "2026-01-02 16:15": {
+            "ETP": {
+                "bid_eur": None,
+                "ask_eur": None,
+            },
+            "ETH": {
+                "bid_eur": 2610,
+                "ask_eur": 2613,
+                "local_high_eur": 4000,
             }
         }
+    }
+}
